e0c1d9817465fab3217135c01b286171ef1ad8ff
Sergio Peralta <seperalta88@gmail.com>
2023-09-26 12:27:28 -0300

    Update mobile menu

--- src/components/navigation/MobileMenu.jsx
+++ src/components/navigation/MobileMenu.jsx
@@ -12,7 +12,7 @@ const MobileMenu = () => {
         className='menu-btn absolute top-4 left-4 text-xl z-30'
         onClick={() => setShowMenu(true)}
       >
-        <img src='blackham.svg' alt='logo' className='w-10' />
+        <img src='/blackham.svg' alt='logo' className='w-10' />
       </button>
       {showMenu && (
         <div className='menu fixed inset-0 bg-white z-30 pt-8'>
@@ -21,7 +21,7 @@ const MobileMenu = () => {
             onClick={() => setShowMenu(false)}
             className='absolute top-4 right-4 text-3xl'
           >
-            <img src='xhamb.svg' alt='logo' className='w-10' />
+            <img src='/xhamb.svg' alt='logo' className='w-10' />
           </button>
           <NavLink to='/'>Logo</NavLink>
           <ul className='flex items-stretch text-center flex-col mt-16 px-4'>
