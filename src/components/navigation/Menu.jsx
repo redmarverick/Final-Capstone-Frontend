a844f8a40a220980cb69e8f0c2343f3485c067b7
Sergio Peralta <seperalta88@gmail.com>
2023-09-26 12:27:18 -0300

    Update main menu

--- src/components/navigation/Menu.jsx
+++ src/components/navigation/Menu.jsx
@@ -6,7 +6,7 @@ const Menu = () => (
   <aside className='fixed top-0 bottom-0 text-left navigation hidden border-r border-gray-300 float-left py-8 w-[250px] bg-slate-100 h-screen lg:block'>
     <NavLink to='/'>Logo</NavLink>
     <ul className='flex flex-col mt-16 ml-4 h-[70%]'>
-      <MenuLink to='/'>VEHICLES</MenuLink>
+      <MenuLink to='/cars'>VEHICLES</MenuLink>
       <MenuLink to='/new-reservation'>RESERVE</MenuLink>
       <MenuLink to='/reserved'>MY RESERVATIONS</MenuLink>
       <MenuLink to='/new-car'>ADD/REMOVE CAR</MenuLink>
