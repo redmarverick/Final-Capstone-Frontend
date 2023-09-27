44a2e519649e03831a3d74fc5d0030df6d50886a
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-09-27 11:11:40 +0300

    Create a ReservationsSlice

---
+++ src/redux/reservations/reservationsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const reservationsSlice = createSlice({
+  name: '',
+  initialState: {
+
+  },
+  reducers: {
+
+  }
+})
+
+export default reservationsSlice.reducer;
