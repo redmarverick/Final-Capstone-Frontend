fbc4cc0490ba96f8319df32c956d5f7a484c0fcd
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-09-27 11:49:37 +0300

    Add slice car reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reservationReducer from '../redux/reservations/reservationsSlice'
+import carReducer from '../redux/cars/carsSlice'
 
 export default configureStore({
   reducer: {
-    reservations: reservationReducer
+    reservations: reservationReducer,
+    cars: carReducer
   },
 });
