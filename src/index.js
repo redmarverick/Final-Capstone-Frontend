bd254734c3aeb187ea858dbcaa2ad294213903c1
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-09-28 04:10:24 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import store from './app/store';
+import store from './redux/store';
 import router from './router/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
