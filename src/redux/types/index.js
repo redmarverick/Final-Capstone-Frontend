2324264ff16c2fbe7679427c551e3e083153c345
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-09-28 04:09:14 +0300

    Setup constants to represent action types in the app

---
+++ src/redux/types/index.js
@@ -0,0 +1,19 @@
+const FETCH_RESERVATIONS = 'redux/reservations/FETCH_RESERVATIONS';
+const RESERVE_CAR = 'redux/reservations/RESERVE_cAR';
+const FETCH_CARS = 'redux/cars/FETCH_CARS';
+const ADD_CAR = 'redux/cars/ADD_CAR';
+const DELETE_CAR = 'redux/cars/DELETE_CAR';
+const UPDATE_CAR = 'redux/cars/UPDATE_CAR';
+const SET_FETCHED_USER = 'redux/user/SET_FETCHED_USER';
+const LOGOUT_USER = 'redux/user/LOGOUT_USER';
+
+export default {
+  FETCH_RESERVATIONS,
+  RESERVE_CAR,
+  FETCH_CARS,
+  ADD_CAR,
+  DELETE_CAR,
+  UPDATE_CAR,
+  SET_FETCHED_USER,
+  LOGOUT_USER,
+};
