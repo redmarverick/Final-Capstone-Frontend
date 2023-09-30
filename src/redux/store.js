3f09f3dedb43b75890beb815a22dab31957956f7
Sergio Peralta <seperalta88@gmail.com>
2023-09-30 18:47:58 -0300

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import reservationReducer from './reservations/reservationsSlice';
-import carsReducer from './cars/carsSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import reservationReducer from "./reservations/reservationsSlice";
+import carsReducer from "./cars/carsSlice";
+import userSlice from "./users/userSlice";
 
 const rootReducer = combineReducers({
   cars: carsReducer,
   reservations: reservationReducer,
+  user: userSlice,
 });
 
 const store = configureStore({
