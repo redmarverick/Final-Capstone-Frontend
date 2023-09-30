7ef30a189c1086cc88488b7069112433a66f5ffd
Sergio Peralta <seperalta88@gmail.com>
2023-09-30 19:55:28 -0300

    Update Auth layout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 import car from "../assets/images/car.png";
 import logo from "../assets/images/logo.png";
@@ -5,25 +6,28 @@ import logo from "../assets/images/logo.png";
 const AuthLayout = ({ children }) => (
   <div className='min-h-screen max-h-screen flex flex-col overflow-hidden relative bg-amber-500'>
     {/* Header */}
-    <header className='p-4 flex justify-between items-center'>
+    <header className='z-50 py-4 md:px-10 flex justify-between items-center'>
       {/* Logo (Top Left) */}
-      <div className='text-white text-2xl font-bold flex items-center'>
+      <NavLink
+        to='/cars'
+        className='text-white text-2xl font-bold flex items-center'
+      >
         <img src={logo} alt='Logo' className='h-20 w-20 mr-2' />
-      </div>
+      </NavLink>
       <div className='flex gap-2'>
-        <button
-          type='button'
+        <NavLink
+          to='/login'
           className='z-50 border border-amber-700 text-amber-700 px-4 py-2 rounded-full hover:bg-amber-700 hover:text-white cursor-pointer'
         >
           Login
-        </button>
+        </NavLink>
         {/* SIGNUP Button (Top Right) */}
-        <button
-          type='button'
-          className='bg-white text-amber-700 px-4 py-2 rounded-full'
+        <NavLink
+          to='/signup'
+          className='z-50 bg-white text-amber-700 px-4 py-2 rounded-full hover:bg-amber-700 hover:text-white cursor-pointer'
         >
           Sing Up
-        </button>
+        </NavLink>
       </div>
     </header>
 
