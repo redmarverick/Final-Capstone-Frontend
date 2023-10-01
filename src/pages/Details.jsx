d1f13fb5c66248158bf84a7894647213be277c43
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-10-01 15:20:36 +0300

    Make the details page dynamic

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,33 +1,45 @@
-import { Link, useParams } from "react-router-dom";
-
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCarById } from "../redux/cars/carsSlice";
 import MainLayout from "../layouts/MainLayout";
 
 const Details = () => {
-  // TODO: get the id from the URL to use it in the API call in redux
   const { id } = useParams();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const carById = useSelector((state) => state.cars);
+  const car = carById.cars[0];
 
-  // useEffect(() => {
-  //   dispatch(getCarInfo(id));
-  // }, [dispatch, id]);
+  const navigate = useNavigate();
 
-  const car = {
-    id: 6,
-    name: "Porsche",
-    photo: "/car2.svg",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quas, voluptatum. Quisquam, voluptate. Quas, voluptatum.",
-    city: "New York",
-    country: "USA",
-    price: 100000,
-  };
+  useEffect(() => {
+    dispatch(fetchCarById(id));
+  }, [dispatch, id]);
 
-  const handleReserve = () => {
+  if (!car || carById.status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (carById.status === 'error') {
+    return <div>Error loading car details</div>;
+  }
+
+  const handleReserve = (carObject) => {
     sessionStorage.setItem("id", id);
+    navigate(
+      '/new-reservation',
+      {
+        state: {
+          car: carObject,
+        },
+      },
+    );
   };
 
-  // eslint-disable-next-line
-  const { name, description, photo, price } = car;
+  const {
+    name, photo, description, price,
+  } = car;
+
   return (
     <MainLayout>
       <Link
@@ -42,7 +54,7 @@ const Details = () => {
         <div className='mx-auto flex-nowrap w-72 h-72 relative flex flex-col justify-center md:w-[45%] md:h-[45%]'>
           <img
             src={photo}
-            alt={`${name}`}
+            alt={name}
             className='z-10 rounded-full md:mb-20'
           />
         </div>
@@ -51,8 +63,6 @@ const Details = () => {
             {name}
           </h1>
           <hr className='w-24 self-center border-t-white md:w-52 md:my-2' />
-          {/* // TODO: add the rest of the information from the car object,
-          refactor in a smart way */}
           <ul className='flex flex-col'>
             <li className='flex justify-between p-3 bg-gray-200'>
               <span className='text-sm font-light'>Price</span>
@@ -75,20 +85,19 @@ const Details = () => {
             {description}
           </p>
           <Link to='/cars' className='text-sm mt-4 self-end'>
-            {/* eslint-disable-next-line */}
-            BROWSE MORE MODELS <span className='text-[#96BF0E]'>&#10148;</span>
+            BROWSE MORE MODELS
+            <span className='text-[#96BF0E]'>&#10148;</span>
           </Link>
           <div className='flex flex-col items-center justify-between'>
-            <Link
-              to='/new-reservation'
+            <button
               type='button'
               className='w-48 flex justify-between items-center md:mt-16 mt-8 text-center text-white bg-[#96BF0E] border-0 py-2 px-4 focus:outline-none hover:bg-[#7da60a] rounded-full text-lg'
-              onClick={handleReserve}
+              onClick={() => handleReserve(car)}
             >
               <img src='/booking.svg' alt='calendar' className='mr-2' />
               Reserve
               <img src='/circlearrow.svg' alt='calendar' className='ml-2' />
-            </Link>
+            </button>
           </div>
         </div>
       </div>
