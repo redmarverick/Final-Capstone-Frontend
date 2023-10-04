6886fd2498c30660315429ae3bbcd75321fe4467
Sergio Peralta <seperalta88@gmail.com>
2023-10-04 17:54:49 -0300

    Update homepage styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import car from "../assets/images/car.png";
 import logo from "../assets/images/logo.png";
 
@@ -8,37 +8,39 @@ const HomePage = () => (
     <header className='p-4 flex justify-between items-center'>
       {/* Logo (Top Left) */}
       <div className='text-white text-2xl font-bold flex items-center'>
-        <img src={logo} alt='Logo' className='h-20 w-20 mr-2' />
+        <img src={logo} alt='Logo' className='h-24 w-24 mr-2' />
       </div>
       <div className='flex gap-2'>
-        <Link
+        <NavLink
           to='/login'
           className='z-50 border border-amber-700 text-amber-700 px-4 py-2 rounded-full hover:bg-amber-700 hover:text-white cursor-pointer'
         >
           Login
-        </Link>
+        </NavLink>
         {/* SIGNUP Button (Top Right) */}
-        <button
-          type='button'
-          className='bg-white text-amber-700 px-4 py-2 rounded-full'
+        <NavLink
+          to='/signup'
+          className='z-50 bg-white text-amber-700 px-4 py-2 rounded-full hover:bg-amber-700 hover:text-white cursor-pointer'
         >
           Sing Up
-        </button>
+        </NavLink>
       </div>
     </header>
 
     {/* Main Content */}
-    <main className=' flex-grow text-center z-10'>
-      <h2 className=' text-4xl text-white font-semibold mb-4'>
-        CAR BOOKING APP
-      </h2>
-      <Link
-        to='/cars'
-        className='bg-lime-500 font-bold text-white px-4
+    <main className=' flex-grow text-center flex items-center justify-center z-10'>
+      <div className='flex-col gap-8 mb-40'>
+        <h2 className=' text-4xl md:text-6xl text-white font-semibold mb-4 md:mb-8'>
+          CAR BOOKING APP
+        </h2>
+        <NavLink
+          to='/cars'
+          className='bg-lime-500 font-bold text-white px-4 md:text-xl
         py-2 rounded-md hover:bg-lime-600'
-      >
-        Explore
-      </Link>
+        >
+          Explore
+        </NavLink>
+      </div>
     </main>
     <div className='absolute inset-0 flex items-center justify-center'>
       <img className='h-40vh w-auto' src={car} alt='background' />
