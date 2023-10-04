868302dfd30cff62afa9cbcc87578e743a441c5f
Sergio Peralta <seperalta88@gmail.com>
2023-10-04 17:02:33 -0300

    Add socials

--- src/components/navigation/Menu.jsx
+++ src/components/navigation/Menu.jsx
@@ -2,6 +2,10 @@ import { NavLink } from "react-router-dom";
 import propTypes from "prop-types";
 import MenuLink from "./MenuLink";
 import logo from "../../assets/images/logo.png";
+import facebook from "../../assets/images/facebook.svg";
+import x from "../../assets/images/x.png";
+import instagram from "../../assets/images/instagram.svg";
+import youtube from "../../assets/images/youtube.svg";
 
 const Menu = () => (
   <aside className='fixed top-0 bottom-0 text-left navigation hidden border-r border-gray-300 float-left pb-8 w-[250px] bg-slate-100 h-screen lg:block'>
@@ -15,6 +19,40 @@ const Menu = () => (
       <MenuLink to='/new-car'>ADD CAR</MenuLink>
       <MenuLink to='/delete-car'>REMOVE CAR</MenuLink>
     </ul>
+    <div className='flex items-center justify-center mt-8 gap-2'>
+      <a
+        href='https://www.facebook.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img src={facebook} alt='facebook' className='w-6 h-6' />
+      </a>
+      <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer'>
+        <img src={x} alt='twitter' className='w-4 h-4' />
+      </a>
+      <a
+        href='https://www.youtube.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img src={youtube} alt='youtube' className='w-7 h-7' />
+      </a>
+      <a
+        href='https://www.instagram.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img src={instagram} alt='instagram' className='w-5 h-5' />
+      </a>
+    </div>
+    {/* copyright */}
+    <div className='flex items-center justify-center pt-4 pb-20'>
+      <p className='text-xs text-gray-500 text-center'>
+        &copy; 2023 Car Booking App.
+        <br />
+        All rights reserved.
+      </p>
+    </div>
   </aside>
 );
 
